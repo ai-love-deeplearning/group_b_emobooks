e039b276d757ee72fb360c4dbcd36e0b06886d57
Kentaro Kotaki <ken24693857@icloud.com>
2019-09-10 22:34:03 +0900

    各感情での辞書検索結果をリストに追加する処理をコーディング

--- emobooks_back/word-analyze.py
+++ emobooks_back/word-analyze.py
@@ -22,47 +22,53 @@ def score_analyze(emo_dict, score_list, list_file, list_line, not_file):
           str(i - 1) + ' word is not found in vocabrary.\n')
 
 
-file_1 = open('dic_1.txt', 'r')
-file_2 = open('dic_2.txt', 'r')
-file_3 = open('dic_3.txt', 'r')
-file_4 = open('dic_4.txt', 'r')
+def main():
+    file_1 = open('dic_1.txt', 'r')
+    file_2 = open('dic_2.txt', 'r')
+    file_3 = open('dic_3.txt', 'r')
+    file_4 = open('dic_4.txt', 'r')
 
-line_1 = file_1.readline().rstrip('\n')
-line_2 = file_2.readline().rstrip('\n')
-line_3 = file_3.readline().rstrip('\n')
-line_4 = file_4.readline().rstrip('\n')
+    line_1 = file_1.readline().rstrip('\n')
+    line_2 = file_2.readline().rstrip('\n')
+    line_3 = file_3.readline().rstrip('\n')
+    line_4 = file_4.readline().rstrip('\n')
 
-list_count = 0
-with open('word_list_completed.txt') as f:
-    for line in f:
-        list_count += 1
+    list_count = 0
+    with open('pre-text.txt') as f:
+        for line in f:
+            list_count += 1
 
-like = {}
-Anger = {}
-sorrow = {}
-fun = {}
+    happy = {}
+    angry = {}
+    sad = {}
+    fun = {}
 
-score_like = []
-score_Anger = []
-score_sorrow = []
-score_fun = []
+    score_happy = []
+    score_angry = []
+    score_sad = []
+    score_fun = []
 
-list_file = open('pre3.txt', 'r')
-list_line = list_file.readline().rstrip('\n')
+    list_file = open('pre-text.txt', 'r')
+    list_line = list_file.readline().rstrip('\n')
 
-not_file = open('not_vocab_word.txt', 'a')
+    not_file = open('not_vocab_word.txt', 'a')
 
-set_dictionary(like, file_1, line_1)
-set_dictionary(Anger, file_2, line_2)
-set_dictionary(sorrow, file_3, line_3)
-set_dictionary(fun, file_4, line_4)
+    set_dictionary(happy, file_1, line_1)
+    set_dictionary(angry, file_2, line_2)
+    set_dictionary(sad, file_3, line_3)
+    set_dictionary(fun, file_4, line_4)
 
-score_analyze(like, score_like, list_file, list_line, not_file)
+    score_analyze(happy, score_happy, list_file, list_line, not_file)
+    score_analyze(angry, score_angry, list_file, list_line, not_file)
+    score_analyze(sad, score_sad, list_file, list_line, not_file)
+    score_analyze(fun, score_fun, list_file, list_line, not_file)
+
+    file_1.close()
+    file_2.close()
+    file_3.close()
+    file_4.close()
+
+    not_file.close()
 
 
-file_1.close()
-file_2.close()
-file_3.close()
-file_4.close()
-
-print(str(list_count))
+main()
