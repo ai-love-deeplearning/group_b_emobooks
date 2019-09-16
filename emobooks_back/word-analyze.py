cde17d281f0009c89d176dc273a31094d45abeb4
Kentaro Kotaki <ken24693857@icloud.com>
2019-09-16 11:02:42 +0900

    text_editとword_analyzeの結合

--- emobooks_back/word-analyze.py
+++ emobooks_back/word-analyze.py
@@ -1,3 +1,48 @@
+import MeCab
+
+
+def cut_text(edit_file):
+    with open(edit_file, 'r') as f:
+        memory_line = []
+        for line in f:
+            memory_line.append(line.strip())
+
+    with open('pre-text.txt', 'w') as f:
+        for i in memory_line:
+            f.write(i)
+
+
+def rewrite_with_wakati(memory_line, edit_file):
+    with open(edit_file, 'r') as f:
+        memory_line = f.readline()
+
+    mecab = MeCab.Tagger("-Owakati")
+    memory_line = mecab.parse(memory_line)
+
+    with open('pre-text.txt', 'w') as f:
+        f.write(memory_line)
+
+
+def remove_text(memory_line, edit_file):
+    with open(edit_file, 'r') as f:
+        memory_line = f.readline()
+
+    memory_line = memory_line.replace('「', '').replace(
+        '」', '').replace('.', '').replace('・', '').replace('…', '')
+
+    with open('pre-text.txt', 'w') as f:
+        f.write(memory_line)
+
+
+def separate_word(memory_line, edit_file):
+    with open(edit_file, 'r') as f:
+        memory_line = f.readline()
+        text = memory_line.replace(' ', '\n')
+
+    with open('pre-text.txt', 'w') as f:
+        f.write(text)
+
+
 def set_dictionary(emo_dict, dict_file, dict_line):
     while dict_line:
         item = dict_line.split(', ')[0]
@@ -53,7 +98,26 @@ def create_emotion_file(score_analyze):
             emo += 1
 
 
-def main():
+def text_edit_main():
+    memory_line = ''
+    edit_file = 'pre-text.txt'
+    cut_text(edit_file)
+    rewrite_with_wakati(memory_line, edit_file)
+    remove_text(memory_line, edit_file)
+    separate_word(memory_line, edit_file)
+    with open('pre-text.txt', 'r', encoding='utf-8') as f:
+        line = f.readlines()
+        memory_list = []
+        for i in line:
+            if i != '\n':
+                memory_list.append(i)
+
+    with open('pre-text.txt', 'w', encoding='utf-8') as f:
+        for r in memory_list:
+            f.write(r)
+
+
+def create_emo_data_main():
     file_1 = open('dic_happy.txt', 'r')
     file_2 = open('dic_angry.txt', 'r')
     file_3 = open('dic_sad.txt', 'r')
@@ -121,6 +185,11 @@ def main():
     sum_value(analyzed_array[3], score_rength[3], score_fun)
 
     create_emotion_file(analyzed_array)
+
+
+def main():
+    text_edit_main()
+    create_emo_data_main()
 
 
 main()
