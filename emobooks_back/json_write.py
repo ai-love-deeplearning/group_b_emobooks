08e90ad569601641a32d86768a50552909e61aa0
Kentaro Kotaki <ken24693857@icloud.com>
2019-09-17 13:23:48 +0900

    jsonファイルの作成と編集をするプログラムの完成

--- emobooks_back/json_write.py
+++ emobooks_back/json_write.py
@@ -2,8 +2,11 @@ import json
 
 
 def main():
-    with open('sample_test.json') as f:
+    with open('sample_test.json', encoding='utf-8') as f:
         df = json.load(f)
+        df['n7105co']['data'] = [[0, 1], [2, 3]]
+    with open('sample_test.json', 'w', encoding='utf-8') as f:
+        json.dump(df, f, indent=4, ensure_ascii=False)
 
-    print(df['n7105co'])
+
 main()
