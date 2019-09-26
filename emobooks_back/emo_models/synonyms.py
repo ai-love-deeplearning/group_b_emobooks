acb498cebe76541e16d86401aecbfe617152c234
Kentaro Kotaki <ken24693857@icloud.com>
2019-09-26 19:32:38 +0900

    no message

--- emobooks_back/emo_models/synonyms.py
+++
@@ -1,9 +0,0 @@
-from gensim.models import word2vec
-import pprint
-
-model = word2vec.Word2Vec.load("emo_2.model")
-results = model.wv.most_similar(positive=['猫'])
-for result in results:
-    print(result)
-
-# pprint.pprint(model.similarity('怒り', '激怒'))#２単語間の単語の距離を出す
