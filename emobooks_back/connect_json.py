3429633c22fb5f9ca1b68f5128cd1d3a7c865193
Kentaro Kotaki <ken24693857@icloud.com>
2019-09-27 17:33:15 +0900

    #2 リスト形式のjsonに対応

--- emobooks_back/connect_json.py
+++ emobooks_back/connect_json.py
@@ -1,14 +1,15 @@
 import json
 import os
 
-with open('yyyyyy.json', 'r') as f:  # 結合したいjsonファイルを指定（こっちが消える）
+with open('test1.json', 'r') as f:  # 結合したいjsonファイルを指定（こっちが消える）
     connect_dict = json.load(f)
 
-with open('yyyyy.json', 'r') as f:  # 結合先のjsonファイルを指定（こっちにくっつく）
-    connect_dict.update(json.load(f))
+with open('test2.json', 'r') as f:  # 結合先のjsonファイルを指定（こっちにくっつく）
+    connect_list = json.load(f)
+    for i in connect_list:
+        connect_dict.append(i)
 
-
-with open('yyyyy.json', 'w') as f:
+with open('test2.json', 'w') as f:
     json.dump(connect_dict, f, indent=4)
 
-os.remove('yyyyyy.json')
+os.remove('test1.json')
