1c93048a72cfbcff8b9e2586a29a40e3d09bb442
DaichiKanazawa <b1016135@fun.ac.jp>
2019-10-04 15:24:06 +0900

    ｋｋｋｋ

---
+++ Emo_Graph_Prototype/app/app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.recyclerview
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //recyclerView_main.setBackgroundColor(Color.BLUE)
+
+        recyclerView_main.layoutManager= LinearLayoutManager(this)
+        recyclerView_main.adapter= MainAdapter()
+
+    }
+
+
+
+}
---
+++ Emo_Graph_Prototype/app/app/src/main/java/com/example/recyclerview/MainAdapter.kt
@@ -0,0 +1,56 @@
+package com.example.recyclerview
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.book_row.view.*
+
+class MainAdapter : RecyclerView.Adapter<CustomViewHolder>() {
+
+    val bookTitles = listOf("First title", "second", "third")
+    val bookAuthors = listOf("First author", "second author", "third author")
+    val bookStorys = listOf("First story", "second story", "third story")
+
+    private var mRecyclerView : RecyclerView? = null
+
+    override fun getItemCount(): Int {
+        return bookTitles.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val layoutiInflater = LayoutInflater.from(parent?.context)
+        val cellForRow = layoutiInflater.inflate(R.layout.book_row, parent, false)
+        return CustomViewHolder(cellForRow)
+
+
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        val bookTitle = bookTitles.get(position)
+        holder.view.book_title.text= bookTitle
+        val bookAuthor = bookAuthors.get(position)
+        holder.view.book_author.text= bookAuthor
+        val bookStory = bookStorys.get(position)
+        holder.view.book_story.text= bookStory
+    }
+
+}
+
+class CustomViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+
+    companion object{
+        val BOOK_TITLE_KEY ="BOOK_TITLE"
+    }
+    init{
+        view.setOnClickListener {
+            val intent=Intent(view.context,DetailActivity::class.java)
+
+            intent.putExtra(BOOK_TITLE_KEY,"a")
+
+            view.context.startActivity(intent)
+        }
+    }
+}
