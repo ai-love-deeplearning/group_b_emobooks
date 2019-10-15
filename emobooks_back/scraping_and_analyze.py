d246f200a076fa5468aeb6d0cf3bf1e9c7d58e9a
Kentaro Kotaki <ken24693857@icloud.com>
2019-10-15 14:34:45 +0900

    スクレイピングコードと感情分析コードの組み合わせ #1

--- emobooks_back/scraping_and_analyze.py
+++ emobooks_back/scraping_and_analyze.py
@@ -1,6 +1,9 @@
-import MeCab
+﻿from bs4 import BeautifulSoup
+from urllib import request
+from urllib.request import urlopen
+import time
 import json
-
+import MeCab
 
 def edit_text(text):
     text = text.replace('\n', '').replace(' ', '').replace('　', '')
@@ -93,6 +96,7 @@ def create_input_data(data):
             text += ','
         edited_data.append(text.rstrip(','))
         i += 1
+    return edited_data
 
 
 def text_edit_main(text):
@@ -145,10 +149,56 @@ def create_emo_data_main(text):
     return analyzed_array
 
 
-def emotion_data():
-    with open('data.txt', 'r') as f:
+def emotion_data(file_name):
+    with open(file_name, 'r') as f:
         text = f.read()
 
     text = text_edit_main(text)
     data = create_emo_data_main(text)
     input_data = create_input_data(data)
+    return input_data
+
+
+def getBaseUlr(jsonfile):
+    with open(jsonfile, 'r', encoding="utf-8") as f:
+        jsonData = json.load(f)
+
+        ncode = []
+        baseUlr = []
+        Ulr = []
+        for nobeldata in jsonData:
+            ncode.append(nobeldata['ncode'])
+
+        print(ncode)
+        for i in ncode:
+            emotion_data_list = []
+            # 作品本文ページのURL
+            Ulr = 'https://ncode.syosetu.com/' + "{}/".format(i)
+            baseUlr.append(Ulr)
+            res = urlopen(Ulr)
+            soup = BeautifulSoup(res, "html.parser")
+            num_parts = len(soup.find_all(class_='subtitle'))
+            textFileName = i + '.txt'
+
+            with open(textFileName, "w", encoding="utf-8") as f:
+                for part in range(1, num_parts + 1):
+                    # 作品本文ページのURL
+                    url = Ulr + "{:d}/".format(part)
+                    res = request.urlopen(url)
+                    soup = BeautifulSoup(res, "html.parser")
+
+                    # CSSセレクタで本文を指定
+                    honbun = soup.select_one("#novel_honbun").text
+
+                    # 保存
+                    f.write(honbun)
+                    print("part {:d} downloaded".format(part))  # 進捗を表示
+                    time.sleep(1)  # 次の部分取得までは1秒間の時間を空
+                else:
+                    f.close()
+            emotion_data_list.append(emotion_data(textFileName))
+            time.sleep(1)
+
+
+if __name__ == '__main__':
+    getBaseUlr('sample.json')
